d0ec030876669ab5853d419c0970cd8b9ff7f0a7
Konos9125 <68736619+Konos9125@users.noreply.github.com>
2020-07-25 20:18:38 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from selenium import webdriver
 import os
 
 
-
+api_key = "Mz4BfkixaTnYgn2ULmT92ZIo1"
+api_secret_key = "EKanjKerxoaMyFSCKomu7PgtKwwrh8r4w8xvbzYu5qn1Ubvh6l"
+access_token = "1286222502923042818-mlVmmJ6rLhiLSsdeKobbxvDCVEySbs"
+secret_access_token = "dmkBry6Crs3Fm1o9IYhpaJRNIvnIhgcFnPnihSHfNwgMl"
 auth = tw.OAuthHandler(api_key, api_secret_key)
 auth.set_access_token(access_token, secret_access_token)
 
@@ -21,7 +24,7 @@ if deaths == "0":
     deaths_announcement = "Δεν υπήρξαν θάνατοι απο τον κορωνοϊό."
 else:
     deaths_announcement = "Σημερινοί θανάτοι: " + deaths
-    
+
 if tests == "0":
     tests_announcement = "."
 else:
@@ -43,17 +46,17 @@ wait = True
 
 while True:
 
-    #loop until its 19:00
+    #loop until its 20:30
     if wait:
         print("Waiting..")
         wait = False
 
     time_ = time.ctime()
 
-    if "20:00:00" in time_:
+    if "20:30:00" in time_:
 
         print ("Running....")
-        
+
         #take a screenshot of the webpage with the covid graph and upload it on twitter
         driver.get('http://fullimg.atwebpages.com')
         time.sleep(5)
@@ -69,7 +72,7 @@ while True:
                     + "\n\nSources:\nhttps://corona.help\nhttps://en.wikipedia.org/wiki/COVID-19_pandemic_in_Cyprus"
                     + "\nΠροσοχή: Υπάρχει περίπτωση οι πληροφορίες να είναι λανθασμένες."
                     + "\n\n\n\u0023covid19 \u0023covidcyprus \u0023menoumespiti"))
-                    
+
         file.close()
 
         #read the file and update status
