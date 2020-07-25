fc4a44cd89bfac93fe299d9f18a695a8c4eb84e9
Konos9125 <68736619+Konos9125@users.noreply.github.com>
2020-07-25 19:16:20 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,10 +5,7 @@ from selenium import webdriver
 import os
 
 
-api_key = "Mz4BfkixaTnYgn2ULmT92ZIo1"
-api_secret_key = "EKanjKerxoaMyFSCKomu7PgtKwwrh8r4w8xvbzYu5qn1Ubvh6l"
-access_token = "1286222502923042818-mlVmmJ6rLhiLSsdeKobbxvDCVEySbs"
-secret_access_token = "dmkBry6Crs3Fm1o9IYhpaJRNIvnIhgcFnPnihSHfNwgMl"
+
 auth = tw.OAuthHandler(api_key, api_secret_key)
 auth.set_access_token(access_token, secret_access_token)
 
@@ -53,7 +50,7 @@ while True:
 
     time_ = time.ctime()
 
-    if "19:00:00" in time_:
+    if "20:00:00" in time_:
 
         print ("Running....")
         
@@ -70,7 +67,9 @@ while True:
                     + "\n\n\n" + deaths_announcement
                     + "\nΣύνολο: "+ str(total) + " κρούσματα."
                     + "\n\nSources:\nhttps://corona.help\nhttps://en.wikipedia.org/wiki/COVID-19_pandemic_in_Cyprus"
+                    + "\nΠροσοχή: Υπάρχει περίπτωση οι πληροφορίες να είναι λανθασμένες."
                     + "\n\n\n\u0023covid19 \u0023covidcyprus \u0023menoumespiti"))
+                    
         file.close()
 
         #read the file and update status
